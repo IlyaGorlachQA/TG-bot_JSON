785f9f7d99e79e9e5573e7c0b052ba8625e0a0fc
Gorlach Ilya (QA Engineer) <Ilya.Gorlach@yandex.ru>
2025-05-02 16:41:49 +0400

    Update main.py

--- tg_json-main/main.py
+++ tg_json-main/main.py
@@ -2,7 +2,7 @@
 from telebot import TeleBot, types
 import json
 
-bot = TeleBot(token='7753946278:AAEpFYv8_xUIceKzCpf6GgTXJ9w4dM17l-U', parse_mode='html') # создание бота
+bot = TeleBot(token='ТОКЕН', parse_mode='html') # создание бота
 
 
 # обработчик команды '/start'
